edadf1ebf5a54202f0a39d438d449b309cc70f19
Huixin <51199014+N3K0521@users.noreply.github.com>
2020-03-30 04:30:51 +0800

    Add files via upload

---
+++ workshop02.py
@@ -0,0 +1,47 @@
+'''
+DO NOT CHANGE THE NAME OF THIS FILE, or else the tester will not work.
+The first function requires that you replace the given strings with
+your personal details. It is important that you enter your student number
+and your student email correctly. If your number and email do not match we
+will then check your name, so your name acts as a failsafe.
+'''
+
+# Student details
+def details():
+    student_number = '31552544' #write your student number as a string
+    student_email = 'hwan0158' + '@student.monash.edu' #write your student email
+    name = 'Huixin Wang' #write your name as it appears on Moodle
+    return str(student_number), student_email, name
+
+
+# Task 1
+def calculate(x, y, operator):
+    pass
+
+
+# Task 2
+def is_leap_year(year):
+    pass
+
+
+def leap_year_answer(year):
+    pass
+
+
+# Task 3
+def next_triangular_number(num):
+    pass
+
+
+# Task 4
+def add(numbers):
+    pass
+
+
+def flip(binary_string):
+    pass
+
+
+# Challenge
+def estimate_e(error):
+    pass
